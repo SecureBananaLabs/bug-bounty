599bb2221aabbdb270a8ce011c0846bda93774e7
tho.nguyen <91511523+haki203@users.noreply.github.com>
2026-05-22 20:19:25 +0700

    Add admin operations panel

--- apps/api/src/controllers/adminController.js
+++ apps/api/src/controllers/adminController.js
@@ -1,6 +1,53 @@
 import { ok } from "../utils/response.js";
-import { getAdminMetrics } from "../services/adminService.js";
+import {
+  decideJobListing,
+  getAdminMetrics,
+  getAuditLog,
+  getDisputes,
+  getModerationJobs,
+  getPlatformControls,
+  getUsers,
+  setPlatformControl,
+  setUserStatus,
+  submitDisputeRuling,
+} from "../services/adminService.js";
 
 export async function metrics(req, res) {
   return ok(res, await getAdminMetrics());
+}
+
+export async function users(req, res) {
+  return ok(res, await getUsers(req.query));
+}
+
+export async function updateUserStatus(req, res) {
+  return ok(res, await setUserStatus(req.params.id, req.body, req.user));
+}
+
+export async function moderationJobs(req, res) {
+  return ok(res, await getModerationJobs(req.query));
+}
+
+export async function decideModerationJob(req, res) {
+  return ok(res, await decideJobListing(req.params.id, req.body, req.user));
+}
+
+export async function disputes(req, res) {
+  return ok(res, await getDisputes(req.query));
+}
+
+export async function ruleDispute(req, res) {
+  return ok(res, await submitDisputeRuling(req.params.id, req.body, req.user));
+}
+
+export async function platformControls(req, res) {
+  return ok(res, await getPlatformControls());
+}
+
+export async function updatePlatformControl(req, res) {
+  return ok(res, await setPlatformControl(req.body, req.user));
+}
+
+export async function audit(req, res) {
+  return ok(res, await getAuditLog(req.query));
 }
