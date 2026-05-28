a0c3a8469da1ba7254fa71ef42af2c59ea724efc
jjb9707 <54659549+jjb9707@users.noreply.github.com>
2026-05-28 21:51:02 +0800

    fix: add authMiddleware to message routes

--- apps/api/src/routes/messageRoutes.js
+++ apps/api/src/routes/messageRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getMessages, postMessage } from "../controllers/messageController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const messageRoutes = Router();
 
-messageRoutes.get("/", getMessages);
-messageRoutes.post("/", postMessage);
+messageRoutes.get("authMiddleware, /", getMessages);
+messageRoutes.post("authMiddleware, /", postMessage);
