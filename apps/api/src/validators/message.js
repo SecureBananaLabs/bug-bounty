d85296e2cf9b82df070b930d18f638325d72a002
jjb9707 <54659549+jjb9707@users.noreply.github.com>
2026-05-28 21:20:31 +0800

    add createMessageSchema

--- apps/api/src/validators/message.js
+++ apps/api/src/validators/message.js
@@ -1,5 +1,3 @@
 import { z } from "zod";
 
-export const createMessageSchema = z.object({
-  // TODO
-});
+export const createMessageSchema = z.object({recipientId: z.string().min(1), content: z.string().min(1).max(5000)});
