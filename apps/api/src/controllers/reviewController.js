d8e6bef9c9023e92f268d505203512ce901e80e8
aronzhan <790637254@qq.com>
2026-05-28 22:10:52 +0800

    fix: add Zod input validation to review creation endpoint

--- apps/api/src/controllers/reviewController.js
+++ apps/api/src/controllers/reviewController.js
@@ -1,10 +1,13 @@
 import { ok } from "../utils/response.js";
 import { createReview, listReviews } from "../services/reviewService.js";
+import { reviewSchema } from "../validators/review.js";
 
 export async function getReviews(req, res) {
   return ok(res, await listReviews());
 }
 
 export async function postReview(req, res) {
-  return ok(res, await createReview(req.body), 201);
+  const payload = reviewSchema.parse(req.body);
+  return ok(res, await createReview(payload), 201);
 }
+
