0470248d1f800234439fa2d500c88d6a8b050edc
jjb9707 <54659549+jjb9707@users.noreply.github.com>
2026-05-28 21:21:03 +0800

    fix: validate reviewController input

--- apps/api/src/controllers/reviewController.js
+++ apps/api/src/controllers/reviewController.js
@@ -1,12 +1,11 @@
+import { createReviewSchema } from "../validators/review.js";
 import { ok } from "../utils/response.js";
 import { createReview, listReviews } from "../services/reviewService.js";
-import { createReviewSchema } from "../validators/review.js";
 
 export async function getReviews(req, res) {
   return ok(res, await listReviews());
 }
 
 export async function postReview(req, res) {
-  const payload = createReviewSchema.parse(req.body);
-  return ok(res, await createReview(payload), 201);
+  return ok(res, await createReview(createReviewSchema.parse(req.body)), 201);
 }
