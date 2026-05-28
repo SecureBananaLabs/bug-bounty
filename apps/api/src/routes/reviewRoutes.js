25aa4c7a36b64c871f75430d1a45afc6b024a636
jjb9707 <54659549+jjb9707@users.noreply.github.com>
2026-05-28 21:50:57 +0800

    fix: add authMiddleware to review routes

--- apps/api/src/routes/reviewRoutes.js
+++ apps/api/src/routes/reviewRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getReviews, postReview } from "../controllers/reviewController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const reviewRoutes = Router();
 
-reviewRoutes.get("/", getReviews);
-reviewRoutes.post("/", postReview);
+reviewRoutes.get("authMiddleware, /", getReviews);
+reviewRoutes.post("authMiddleware, /", postReview);
