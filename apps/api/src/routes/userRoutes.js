540cffb1861373ac8210d6e9fb7808e41969afbd
jjb9707 <54659549+jjb9707@users.noreply.github.com>
2026-05-28 21:51:12 +0800

    fix: add authMiddleware to user routes

--- apps/api/src/routes/userRoutes.js
+++ apps/api/src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getUsers, postUser } from "../controllers/userController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const userRoutes = Router();
 
-userRoutes.get("/", getUsers);
-userRoutes.post("/", postUser);
+userRoutes.get("authMiddleware, /", getUsers);
+userRoutes.post("authMiddleware, /", postUser);
