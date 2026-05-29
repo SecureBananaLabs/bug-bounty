73e2fc83bb92d040708f140e1d0f3cc2d6e7c8c4
Jorel97 <juanjo97r@gmail.com>
2026-05-29 06:41:31 -0600

    Scope proposal list by authenticated user

--- apps/api/src/routes/proposalRoutes.js
+++ apps/api/src/routes/proposalRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getProposals, postProposal } from "../controllers/proposalController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const proposalRoutes = Router();
 
-proposalRoutes.get("/", getProposals);
+proposalRoutes.get("/", authMiddleware, getProposals);
 proposalRoutes.post("/", postProposal);
