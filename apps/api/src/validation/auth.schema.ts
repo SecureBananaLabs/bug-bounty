20df640edd1c14bdb2364979e13f2367e5972cbe
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 03:19:42 -0400

    fix: apply solution for issue #1426

---
+++ apps/api/src/validation/auth.schema.ts
@@ -0,0 +1,7 @@
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().min(2),
+    role: z.enum(['CLIENT', 'FREELANCER']).optional(),
+  }),
+});
