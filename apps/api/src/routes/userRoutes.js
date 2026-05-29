5f6d731a508050f8c44390b70e4879f58110daf4
jjb9707 <54659549+jjb9707@users.noreply.github.com>
2026-05-29 15:02:59 +0800

    fix(#1354): add authMiddleware to user creation

--- apps/api/src/routes/userRoutes.js
+++ apps/api/src/routes/userRoutes.js
@@ -1,17 +1,8 @@
 import { Router } from "express";
 import { getUsers, postUser } from "../controllers/userController.js";
 import { authMiddleware } from "../middleware/auth.js";
-import { userSchema } from "../validators/user.js";
 
 export const userRoutes = Router();
 
 userRoutes.get("/", getUsers);
-userRoutes.post("/", authMiddleware, async (req, res, next) => {
-  try {
-    const payload = userSchema.parse(req.body);
-    req.validatedBody = payload;
-    return postUser(req, res);
-  } catch (err) {
-    return next(err);
-  }
-});
+userRoutes.post("/", authMiddleware, postUser);
