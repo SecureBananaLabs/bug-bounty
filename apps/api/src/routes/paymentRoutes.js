edbe0eeae7bd60b72abedcc297882c0d41cf544e
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-31 02:11:31 -0400

    fix: apply solution for issue #1772

--- apps/api/src/routes/paymentRoutes.js
+++ apps/api/src/routes/paymentRoutes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { createPayment } from "../controllers/paymentController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { createPayment } from "../controllers/paymentController.js";
 
 export const paymentRoutes = Router();
 
 paymentRoutes.post("/", authMiddleware, createPayment);
-
