52fb8c7e0c26bcc13e4adc20d36b9b1aaf86200b
Errordog2 <zmaxzhao1996@gmail.com>
2026-06-06 07:10:07 +0800

    fix: handle async validation errors

--- apps/api/src/routes/notificationRoutes.js
+++ apps/api/src/routes/notificationRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getNotifications, postNotification } from "../controllers/notificationController.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const notificationRoutes = Router();
 
-notificationRoutes.get("/", getNotifications);
-notificationRoutes.post("/", postNotification);
+notificationRoutes.get("/", asyncHandler(getNotifications));
+notificationRoutes.post("/", asyncHandler(postNotification));
