1dd0898afed4f23fdd4fc9e933541e77f62a6f54
bozicovichsantiago20-oss <290439273+bozicovichsantiago20-oss@users.noreply.github.com>
2026-06-05 03:55:31 -0300

    Protect user and search routes

--- apps/api/src/routes/searchRoutes.js
+++ apps/api/src/routes/searchRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { search } from "../controllers/searchController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const searchRoutes = Router();
 
+searchRoutes.use(authMiddleware);
 searchRoutes.get("/", search);
