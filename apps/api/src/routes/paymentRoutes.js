f9c9d138a60b102e62ffea83edb35570018cd922
LIE28-MAX <liangyining28@gmail.com>
2026-06-08 14:00:35 +0800

    fix: require auth on payment routes
    Closes #1772

--- apps/api/src/routes/paymentRoutes.js
+++ apps/api/src/routes/paymentRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/auth.js";
 import { createPayment } from "../controllers/paymentController.js";
 
 export const paymentRoutes = Router();
 
+paymentRoutes.use(authMiddleware);
 paymentRoutes.post("/", createPayment);
