2fa00587d1349c6b2939a3dc9e94ec5b7402cb95
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 14:28:25 -0400

    fix: apply solution for issue #2770

--- apps/api/src/controllers/userController.js
+++ apps/api/src/controllers/userController.js
@@ -1,10 +1,5 @@
-import { registerUser } from "../services/authService.js";
 import { ok } from "../utils/response.js";
 import { createUser, listUsers } from "../services/userService.js";
-
-// Registration validation schema should be imported
-// For now, assuming it's in a validation file
-// The main fix would be to update the validation and service
 
 export async function getUsers(req, res) {
   return ok(res, await listUsers());
@@ -12,10 +7,4 @@ export async function getUsers(req, res) {
 
 export async function postUser(req, res) {
   return ok(res, await createUser(req.body), 201);
-}
-
-export async function postRegister(req, res) {
-  // This would be the registration endpoint
-  // Implementation would be added to handle registration with fullName validation
-}
 }
