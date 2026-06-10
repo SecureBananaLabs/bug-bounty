61f78a4499a62dcbf1322fd2bdc10e9d663f3f7b
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 12:15:15 -0400

    fix: apply solution for issue #2770

--- apps/api/src/controllers/userController.js
+++ apps/api/src/controllers/userController.js
@@ -1,8 +1,25 @@
 import { ok } from "../utils/response.js";
-import { createUser, listUsers } require("../services/userService.js");
+import { createUser, listUsers } from "../services/userService.js";
+import { registerUser } from "../services/authService.js";
+import { validateRegistration } from "../validators/authValidator.js";
 
 export async function getUsers(req, res) {
   return ok(res, await listUsers());
+}
+
+export async function postUser(req, res) {
+  return ok(res, await createUser(req.body), 201);
+}
+
+export async function register(req, res) {
+  try {
+    const userData = await validateRegistration(req.body);
+    return ok(res, await registerUser(userData), 201);
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message
+    });
+  }
 }
 
 export async function postUser(req, res) {
