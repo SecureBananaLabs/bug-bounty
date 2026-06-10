d5c4b859e52116bbf4339fd9a886b727fe161474
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 12:27:44 -0400

    fix: apply solution for issue #2781

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,2 @@
-export * from "./Button";
-export { Button } from './Button';
-export { Card } from './Card';
-export type { ButtonProps, CardProps } from './types';
-export * from "./Card";
+export * from "./Button.js";
+export * from "./Card.js";
