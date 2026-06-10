11b5afbc7e88a4efaf0972ad23a4229179f93dc9
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 11:21:23 -0400

    fix: apply solution for issue #2781

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,6 @@
 export * from "./Button";
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
 export * from "./Card";
