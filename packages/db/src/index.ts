89f239f5d270121df667c8d9343574145d469d3e
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 12:31:57 -0400

    fix: apply solution for issue #2775

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,1 @@
-export * from "@pr1ma/client";
-
-// Note: The above import will fail as @pr1ma/client is a typo/error.
-// It should be import from "@prisma/client" as below.
 export * from "@prisma/client";
