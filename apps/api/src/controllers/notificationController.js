468bf2d396e8fa06b88d6c55aed2e93f801dc623
SyntaxHQ <syntaxhq.dev@gmail.com>
2026-06-10 02:35:36 -0400

    Bind message and notification identity to auth user

--- apps/api/src/controllers/notificationController.js
+++ apps/api/src/controllers/notificationController.js
@@ -6,5 +6,5 @@ export async function getNotifications(req, res) {
 }
 
 export async function postNotification(req, res) {
-  return ok(res, await createNotification(req.body), 201);
+  return ok(res, await createNotification(req.body, req.user.sub), 201);
 }
