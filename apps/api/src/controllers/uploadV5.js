c9999f3d2fe1dc8edc16c94d8f1610675da1dbbd
BWM0223 <b.anthony9618@gmail.com>
2026-06-25 10:18:49 -0700

    fix(#7701): return 400 when upload endpoint gets no file

---
+++ apps/api/src/controllers/uploadV5.js
@@ -0,0 +1,2 @@
+import{ok,fail}from"../utils/response.js";
+export async function uploadFile(req,res){if(!req.file)return fail(res,"No file uploaded",400);return ok(res,{filename:req.file.originalname,size:req.file.size},201);}
