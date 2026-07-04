8257ddaf280bd5c0bf875638275c5c44e26875bd
namdamdoi68-oss <namdamdoi68@gmail.com>
2026-07-04 23:29:14 +0700

    fix(users): validate payload input using Zod createUserSchema

--- apps/api/src/controllers/userController.js
+++ apps/api/src/controllers/userController.js
@@ -1,10 +1,16 @@
 import { ok } from "../utils/response.js";
 import { createUser, listUsers } from "../services/userService.js";
+import { createUserSchema } from "../validators/user.js";
 
 export async function getUsers(req, res) {
   return ok(res, await listUsers());
 }
 
-export async function postUser(req, res) {
-  return ok(res, await createUser(req.body), 201);
+export async function postUser(req, res, next) {
+  try {
+    const parsed = createUserSchema.parse(req.body);
+    return ok(res, await createUser(parsed), 201);
+  } catch (error) {
+    next(error);
+  }
 }
