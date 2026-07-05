c7a7295b2bbe94a00c0c1f7efeaf4f935feef832
Qiyao Tan <tanqiyao0213@gmail.com>
2026-07-05 21:00:41 +0100

    Validate user creation payloads

--- apps/api/src/controllers/userController.js
+++ apps/api/src/controllers/userController.js
@@ -1,10 +1,16 @@
-import { ok } from "../utils/response.js";
+import { ok, fail } from "../utils/response.js";
 import { createUser, listUsers } from "../services/userService.js";
+import { createUserSchema } from "../validators/user.js";
 
 export async function getUsers(req, res) {
   return ok(res, await listUsers());
 }
 
 export async function postUser(req, res) {
-  return ok(res, await createUser(req.body), 201);
+  const payload = createUserSchema.safeParse(req.body);
+  if (!payload.success) {
+    return fail(res, "Invalid user payload", 400);
+  }
+
+  return ok(res, await createUser(payload.data), 201);
 }
