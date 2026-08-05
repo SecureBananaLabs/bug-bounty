aa9abb1dd90addf04e39de5a149299e21dd52f82
Mattia <mattia@iMac-di-Mattia.homenet.telecomitalia.it>
2026-08-06 01:22:45 +0200

    fix: protect issue #2773 route with auth middleware

--- apps/api/src/routes/proposalRoutes.js
+++ apps/api/src/routes/proposalRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getProposals, postProposal } from "../controllers/proposalController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const proposalRoutes = Router();
 
 proposalRoutes.get("/", getProposals);
-proposalRoutes.post("/", postProposal);
+proposalRoutes.post("/", authMiddleware, postProposal);
