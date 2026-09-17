3634fdeb64d0c2c386346672f8ce794f592bec9f
slsgzs-cloud <slsgzs@gmail.com>
2026-09-17 13:06:47 +0800

    fix: restrict proposal creation to freelancers (fixes #12523)

--- apps/api/src/routes/proposalRoutes.js
+++ apps/api/src/routes/proposalRoutes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { authMiddleware, requireRole } from "../middleware/auth.js";
-import { createProposal, getProposals, approveProposal } from "../controllers/proposalController.js";
+import { getProposals, postProposal } from "../controllers/proposalController.js";
 
 export const proposalRoutes = Router();
 
 proposalRoutes.use(authMiddleware);
-proposalRoutes.post("/", requireRole("freelancer"), createProposal);
 proposalRoutes.get("/", getProposals);
-proposalRoutes.post("/:id/approve", requireRole("client"), approveProposal);
+proposalRoutes.post("/", requireRole("freelancer"), postProposal);
