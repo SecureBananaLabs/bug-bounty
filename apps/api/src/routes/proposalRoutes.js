acea1688f3cd3af726b9985553cf8fa6e36f76a6
slsgzs-cloud <slsgzs@gmail.com>
2026-09-17 13:05:34 +0800

    fix: restrict proposal routes by role (fixes #12523)

--- apps/api/src/routes/proposalRoutes.js
+++ apps/api/src/routes/proposalRoutes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { getProposals, postProposal } from "../controllers/proposalController.js";
+import { authMiddleware, requireRole } from "../middleware/auth.js";
+import { createProposal, getProposals, approveProposal } from "../controllers/proposalController.js";
 
 export const proposalRoutes = Router();
 
+proposalRoutes.use(authMiddleware);
+proposalRoutes.post("/", requireRole("freelancer"), createProposal);
 proposalRoutes.get("/", getProposals);
-proposalRoutes.post("/", postProposal);
+proposalRoutes.post("/:id/approve", requireRole("client"), approveProposal);
