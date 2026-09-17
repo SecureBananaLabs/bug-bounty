be7aecd334d3c5ddee8e0bf1d32159a98173cd22
slsgzs-cloud <slsgzs@gmail.com>
2026-09-18 00:01:03 +0800

    fix: add request ID middleware (fixes #12623)

--- apps/api/src/middleware/rateLimit.js
+++ apps/api/src/middleware/rateLimit.js
@@ -6,3 +6,10 @@ export const apiLimiter = rateLimit({
   standardHeaders: "draft-7",
   legacyHeaders: false
 });
+
+export const requestId = (req, res, next) => {
+  const id = req.headers["x-request-id"] ?? require("crypto").randomUUID();
+  req.id = id;
+  res.setHeader("X-Request-Id", id);
+  next();
+};
