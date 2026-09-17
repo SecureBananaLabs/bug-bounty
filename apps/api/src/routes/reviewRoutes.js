c7ca708827f92d890085c4de0c41392b68addbbc
slsgzs-cloud <slsgzs@gmail.com>
2026-09-17 13:15:04 +0800

    fix: add auth middleware to review routes (fixes #12529)

--- apps/api/src/routes/reviewRoutes.js
+++ apps/api/src/routes/reviewRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getReviews, postReview } from "../controllers/reviewController.js";
 import { authMiddleware } from "../middleware/auth.js";
+import { getReviews, postReview } from "../controllers/reviewController.js";
 
 export const reviewRoutes = Router();
 
+reviewRoutes.use(authMiddleware);
+
 reviewRoutes.get("/", getReviews);
-reviewRoutes.post("/", authMiddleware, postReview);
+reviewRoutes.post("/", postReview);
